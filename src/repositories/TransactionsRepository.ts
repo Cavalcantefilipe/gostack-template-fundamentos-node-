0eb02ea34bb9b9c3171bb3015f96f0cd69193aad
Filipe Alves Cavalcante <filipe.alvescavalcante@gmail.com>
2020-08-28 12:11:39 -0300

    Done

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -5,6 +5,11 @@ interface Balance {
   outcome: number;
   total: number;
 }
+interface CreateTransactionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
 
 class TransactionsRepository {
   private transactions: Transaction[];
@@ -14,15 +19,57 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    let income: number | undefined;
+    let outcome: number | undefined;
+
+    let mapIncome = this.transactions.map((item) => {
+      if (item.type == "income") {
+        return item.value
+      }
+    });
+
+    if (mapIncome.length < 1) {
+      income = 0;
+    } else {
+      income = mapIncome.reduce((a = 0, b) => {
+        return b != undefined ? a + b : a
+      })
+    }
+
+    let mapOutcome = this.transactions.map((item) => {
+      if (item.type == "outcome") {
+        return item.value
+      }
+    });
+
+    if (mapOutcome.length < 1) {
+      outcome = 0
+    } else {
+      outcome = mapOutcome.reduce((a = 0, b) => {
+        return b != undefined ? a + b : a
+      })
+    }
+    income = income != undefined ? income : 0;
+    outcome = outcome != undefined ? outcome : 0;
+    const balanced = {
+      income: income,
+      outcome: outcome,
+      total: income - outcome
+    }
+
+    return balanced;
+
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
